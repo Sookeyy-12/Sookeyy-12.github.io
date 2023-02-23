a0f9e0f5299b5537d6ebc9454abe40a506dbbf47
Sookeyy-12 <thatonesuket@gmail.com>
2023-02-23 19:55:48 +0530

    added a script for logo and progressed the site

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function(){ 
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h2").onmouseover = event => {
+let iterations = 0;
+const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+    if (index < iterations) {
+        return event.target.dataset.value[index];
+    }
+    return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+    if (iterations >= event.target.dataset.value.length) {
+    clearInterval(interval);
+    }
+    iterations += 1 / 3;
+}, 30);
+}
+};
