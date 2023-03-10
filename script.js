593c04d5a1d7cb4c8efab3dd43989e0581ebb729
Sookeyy-12 <thatonesuket@gmail.com>
2023-03-10 06:20:10 +0530

    added pallete icon to switch b/w dark & light mode

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){ 
+/*window.onload = function(){ 
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h2").onmouseover = event => {
@@ -19,3 +19,35 @@ const interval = setInterval(() => {
 }, 30);
 }
 };
+*/
+
+let flag = 1;
+let variable = document.querySelector(':root');
+
+const changeTheme = function() {
+    let vs = getComputedStyle(variable);
+    if (flag === 1) {
+        variable.style.setProperty('--active-bg', vs.getPropertyValue('--dark-mode-bg'));
+        variable.style.setProperty('--active-font', vs.getPropertyValue('--dark-mode-font'));
+        variable.style.setProperty('--active-header', vs.getPropertyValue('--dark-mode-header'));
+        variable.style.setProperty('--active-nav-pan', vs.getPropertyValue('--dark-mode-nav-pan'));
+        variable.style.setProperty('--active-nav-bg', vs.getPropertyValue('--dark-mode-nav-bg'));
+        variable.style.setProperty('--active-nav-border', vs.getPropertyValue('--dark-mode-nav-border'));
+        variable.style.setProperty('--active-digi-pan', vs.getPropertyValue('--dark-mode-digi-pan'));
+        variable.style.setProperty('--active-socs', vs.getPropertyValue('--dark-mode-socs'));
+        flag = 0;
+    } else {
+        variable.style.setProperty('--active-bg', vs.getPropertyValue('--light-mode-bg'));
+        variable.style.setProperty('--active-font', vs.getPropertyValue('--light-mode-font'));
+        variable.style.setProperty('--active-header', vs.getPropertyValue('--light-mode-header'));
+        variable.style.setProperty('--active-nav-pan', vs.getPropertyValue('--light-mode-nav-pan'));
+        variable.style.setProperty('--active-nav-bg', vs.getPropertyValue('--light-mode-nav-bg'));
+        variable.style.setProperty('--active-nav-border', vs.getPropertyValue('--light-mode-nav-border'));
+        variable.style.setProperty('--active-digi-pan', vs.getPropertyValue('--light-mode-digi-pan'));
+        variable.style.setProperty('--active-socs', vs.getPropertyValue('--light-mode-socs'));
+        flag = 1;
+    }
+}
+
+let pallete = document.getElementById('theme');
+pallete.onclick = changeTheme;
