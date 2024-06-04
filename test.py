c63d2157954e6f6c7b48eb6703c738500ecebff1
Sookeyy-12 <thatonesuket@gmail.com>
2024-06-04 22:02:22 +0530

    updated landing page

---
+++ test.py
@@ -0,0 +1,8 @@
+def introduce_self():
+    """Prints a message about a CS student with ML interests."""
+    print("pre_final_year_CSE_student(KIIT_University)")
+    print("fascinated_by(building_creative_solutions_with_code())")
+    print("current_focus(machine_learning())")
+    print("eager_to_leverage_skills(developing_innovative_solutions())")
+
+introduce_self()
