4a9f10a4fb26b824af969ecf5ebefa6ae1cc8b51
Sookeyy-12 <thatonesuket@gmail.com>
2024-12-20 03:01:46 +0530

    threenull

--- components/ui/canvas-reveal-effect.tsx
+++ components/ui/canvas-reveal-effect.tsx
@@ -190,7 +190,7 @@ const ShaderMaterial = ({
     uniforms: Uniforms;
 }) => {
     const { size } = useThree();
-    const ref = useRef<THREE.Mesh>();
+    const ref = useRef<THREE.Mesh>(null);
     let lastFrameTime = 0;
 
     useFrame(({ clock }) => {
